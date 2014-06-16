0b6a8145bddda5840bac553d28dfd42512e326a2
David Findley <david.findley@ou.edu>
2014-06-16 15:20:58 -0500

    A Collection now stores its computers as a sorted slice of strigs

--- server/collection.go
+++ server/collection.go
@@ -1,29 +1,48 @@
 package main
 
-import "github.com/petar/GoLLRB/llrb"
+import "sort"
 
 type Collection struct {
 	Name      string
 	UID       uint32
-	Computers *llrb.LLRB
+	Computers []string
 }
 
 func NewCollection(name string, uid uint32) *Collection {
 	c := &Collection{
 		Name:      name,
 		UID:       uid,
-		Computers: llrb.New(),
+		Computers: make([]string, 0),
 	}
 	return c
 }
 
 func (col *Collection) AddComputer(name string) {
-	col.Computers.ReplaceOrInsert(llrb.String(name))
+	col.Computers = append(col.Computers, name)
+	sort.Strings(col.Computers)
 	wsHub.broadcast <- []byte("Adding new computer, " + name + " into collection, " + col.Name)
 }
 
-func (col *Collection) RemoveComputer(name string) {
-	if (col.Computers.Delete(llrb.String(name)) != nil) {
-		wsHub.broadcast <- []byte("Removing computer, " + name + " from collection, " + col.Name)
+func (col *Collection) RemoveComputer(name string) bool {
+	result := sort.SearchStrings(col.Computers, name)
+	if col.Computers[result] == name {
+		col.Computers = append(col.Computers[:result], col.Computers[result+1:]...)
+		return true
+	} else {
+		return false
+	}
+}
+
+func (col *Collection) ContainsComputer(name string) bool {
+	if result := sort.SearchStrings(col.Computers, name); col.Computers[result] == name {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+func (col *Collection) PrintComputers() {
+	for _,c := range(col.Computers) {
+		println(c)
 	}
 }
