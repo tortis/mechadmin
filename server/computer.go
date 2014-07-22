4721c883885784439a45f14aa16da375336e8a74
David Findley <david.findley@ou.edu>
2014-07-22 12:44:09 -0500

    Update collections when computers status updates.

--- server/computer.go
+++ server/computer.go
@@ -5,9 +5,16 @@ import "github.com/tortis/mechadmin/types"
 type Computer struct {
 	Info types.Status
 	UID  uint32
+	Cols []uint32
 }
 
 func (comp *Computer) GenerateHTML() string {
 	h := `<li class="computer-item">` + comp.Info.CN + `</li>`
 	return h
+}
+
+func (comp *Computer) NotifyCollections() {
+	for _, cid := range comp.Cols {
+		ColStore.Get(cid).UpdateComputer(&comp.Info)
+	}
 }
